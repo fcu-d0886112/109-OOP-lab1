e4cac639fdbfa1ddc25bc6bc47e33cd2fee45b4c
user <user@EC05>
2020-09-29 09:35:27 +0800

    first

---
+++ src/oop/lab1.java
@@ -0,0 +1,10 @@
+package oop;
+
+public class lab1 {
+	
+	public static void main(String[] args) {
+		System.out.println("I'm ªôµµãj");
+		System.out.println("My nid is D0886112");
+		System.out.println("This is lab 1 hw");
+	}
+}

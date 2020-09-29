d6e9514aa92e07b6d9077c37aa3e9008cd17e4fc
user <user@EC05>
2020-09-29 09:39:57 +0800

    english name

--- src/oop/lab1.java
+++ src/oop/lab1.java
@@ -3,7 +3,7 @@ package oop;
 public class lab1 {
 	
 	public static void main(String[] args) {
-		System.out.println("I'm ªôµµãj");
+		System.out.println("I'm judy-chiu");
 		System.out.println("My nid is D0886112");
 		System.out.println("This is lab 1 hw");
 	}
